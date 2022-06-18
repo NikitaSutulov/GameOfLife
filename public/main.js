d44db2484a4d6e89434fff34eb46f407e7a35af7
Nikita Sutulov <notsid212@gmail.com>
2022-06-18 03:16:10 +0300

    Added tile size selector and finally began writing the code.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,33 @@
 'use strict';
-const canvas = document.getElementById('canvas');
-const canvasContext = canvas.getContext('2d');
 
+
+const uiElements = {
+  statsParagraph: document.getElementById('statsPar'),
+  buttons: document.getElementsByClassName('button'),
+  canvas: document.getElementById('canvas'),
+  speedParagraph: document.getElementById('speedPar'),
+  speedSlider: document.getElementById('speedSlider')
+};
+
+const simulate = () => {
+  console.log(uiElements.speedSlider.value);
+};
+
+let simulationFrameInterval = setInterval(simulate, 100 * uiElements.speedSlider.value);
+
+const updateGameSpeed = (gameFrameInterval, callback) => {
+  let currentSpeed = uiElements.speedSlider.value;
+  const checkInterval = () => {
+    setInterval(() => {
+      if (currentSpeed !== uiElements.speedSlider.value)
+      {
+        clearInterval(gameFrameInterval);
+        gameFrameInterval = setInterval(callback, 100 * uiElements.speedSlider.value);
+        currentSpeed = uiElements.speedSlider.value;
+      }
+    }, 50);
+  };
+  checkInterval();
+};
+
+updateGameSpeed(simulationFrameInterval, simulate);
